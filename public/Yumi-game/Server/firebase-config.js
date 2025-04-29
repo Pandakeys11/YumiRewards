e6447245c7e5143bec082854ae23678ea8d638ae
ritz.jm10 <ritz.jm10@gmail.com>
2025-04-29 18:27:22 +0000

    Sync with public cloud folder

--- public/Yumi-game/Server/firebase-config.js
+++ public/Yumi-game/Server/firebase-config.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+// Your web app's Firebase configuration \n
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAN-37ysgFJlncUZAVH7k79HPSqUZy4FKA",
   authDomain: "yumi-pop.firebaseapp.com",
@@ -15,8 +16,7 @@ const firebaseConfig = {
   messagingSenderId: "328717127716",
   appId: "1:328717127716:web:9c7b5beefda761f8ae48b1",
   measurementId: "G-HHDJD3F9VY"
-};
-
+ };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
